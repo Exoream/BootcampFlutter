ed13f512395b674957018d8d84019bfe61b6a543
Al Hilaluddin <shiigetsu1@gmail.com>
2024-01-27 09:48:40 +0800

    Pengumpulan Tugas 10

---
+++ Tugas-10-Styling/styling/lib/Tugas/Tugas10/LoginScreen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(top: 40),
+              child: const Text(
+                "Sanber Flutter",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                "assets/img/flutter.png",
+                height: 100,
+                width: 100,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Username"),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Password"),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Forgot Password",
+              ),
+            ),
+            Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                style: raisedButtonStyle,
+                child: const Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                const Text("    Does not have Account?"),
+                TextButton(
+                  child: const Text(
+                    "Sign In",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+    foregroundColor: Colors.green[300],
+    backgroundColor: Colors.blue[300],
+    minimumSize: const Size(88, 36),
+    padding: const EdgeInsets.symmetric(horizontal: 16),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(6)),
+    ));
